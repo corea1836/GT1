5cbac34d5154202affe8fe04b476249200e9a1ad
unknown <corea1836@naver.com>
2026-03-24 06:58:54 +0900

    feat: 기본 카메라, EnhancedInput 설정

--- Source/GT1/Characters/GT1Character.cpp
+++ Source/GT1/Characters/GT1Character.cpp
@@ -1,9 +1,34 @@
 #include "Characters/GT1Character.h"
 
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+
 AGT1Character::AGT1Character()
 {
 	PrimaryActorTick.bCanEverTick = true;
-
+	
+	// 카메라 초기화
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(RootComponent);
+	CameraBoom->TargetArmLength = 200.f;
+	CameraBoom->SocketOffset = FVector(0.f, 60.f, 30.f);
+	CameraBoom->SetRelativeRotation(FRotator(-10.f, 0.f, 0.f));
+	
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(CameraBoom);
+	
+	/**
+	 * TODO
+	 * - 다이나믹 앵글
+	 *   - 탐색
+	 *   - 다수 적이 있는 전투
+	 *   - 보스전
+	 *   - 암살
+	 *   - 처형
+	 * - 카메라 렉
+	 */
 }
 
 void AGT1Character::BeginPlay()
@@ -18,9 +43,34 @@ void AGT1Character::Tick(float DeltaTime)
 
 }
 
+void AGT1Character::NotifyControllerChanged()
+{
+	Super::NotifyControllerChanged();
+	
+	if (APlayerController* PC = Cast<APlayerController>(GetController()))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PC-> GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(DefaultMappingContext, 0);
+		}
+	}
+}
+
 void AGT1Character::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		
+	}
+}
+
+void AGT1Character::Move(const FInputActionValue& Values)
+{
+}
+
+void AGT1Character::Look(const FInputActionValue& Values)
+{
 }
 
--- Source/GT1/Characters/GT1Character.h
+++ Source/GT1/Characters/GT1Character.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "InputActionValue.h"
 #include "GameFramework/Character.h"
 #include "GT1Character.generated.h"
 
@@ -8,6 +9,22 @@ UCLASS()
 class GT1_API AGT1Character : public ACharacter
 {
 	GENERATED_BODY()
+	
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera, meta=(AllowPrivateAccess=true))
+	TObjectPtr<class USpringArmComponent> CameraBoom;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera, meta=(AllowPrivateAccess=true))
+	TObjectPtr<class UCameraComponent> FollowCamera;
+	
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<class UInputMappingContext> DefaultMappingContext;
+	
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<class UInputAction> MoveAction;
+	
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<UInputAction> LookAction;
 
 public:
 	AGT1Character();
@@ -17,7 +34,12 @@ protected:
 
 public:	
 	virtual void Tick(float DeltaTime) override;
+	
+	virtual void NotifyControllerChanged() override;
 
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+protected:
+	void Move(const FInputActionValue& Values);
+	void Look(const FInputActionValue& Values);
 };
